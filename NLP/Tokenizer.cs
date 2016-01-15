fc5af60e929d0c510dc73a5950c95247b819c542
spykill4 <adzikh@hotmail.com>
2016-01-15 14:01:37 -0500

    Created a basic word tokenizer

--- NLP/Tokenizer.cs
+++ NLP/Tokenizer.cs
@@ -36,5 +36,47 @@ namespace NLP
             }
             return sentences.ToArray();
         }
+
+        /*
+        Splits a body of text into words, where punctuation acts as separate words for easier processing, excluding apostrophes and hyphens and periods, as periods are supposed to be removed by sentences
+
+        TODO: Keep punctuation involved with titles and abreviations included with the word
+
+        params: string text - text to split into sentences
+        returns: string[] - contains all words split up
+        */
+        public static string[] ByWords(string text)
+        {
+            List<string> words = new List<string>();
+            string nextWord = "";
+            for(int i = 0; i < text.Length; i++)
+            {
+                if(text[i] == ' ')
+                {
+                    if (nextWord != "")
+                    {
+                        words.Add(nextWord);
+                        nextWord = "";
+                    }
+                    continue;
+                }
+                if (text[i] == ',')
+                {
+                    if (nextWord != "")
+                    {
+                        words.Add(nextWord);
+                        nextWord = "";
+                    }
+                    words.Add(",");
+                    continue;
+                }
+                nextWord += text[i];
+            }
+            if(nextWord != "")
+            {
+                words.Add(nextWord);
+            }
+            return words.ToArray();
+        }
     }
 }
